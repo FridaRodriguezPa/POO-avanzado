1078d1cff676c67ff699295e6d6837cf24e83d22
Frida Rodriguez <fridaixchelrp@gmail.com>
2021-10-20 17:12:07 -0500

    Excepciones

--- src/com/generation/Main.java
+++ src/com/generation/Main.java
@@ -6,14 +6,32 @@ import com.generation.paquete2.Animal;
 
 public class Main {
 
+        public static int numerador = 10;
+        public static int denominador = 0;
+        public static float division;
+
     public static void main(String[] args) {
-        Ejercicio person= new Ejercicio("Santiago", 33, "102045455");
-//        person.setName("Santiago");
-//        person.setAge(33);
-//        person.setId("102045455");
-        System.out.println( "Name: " + person.getName());
-        System.out.println( "Age: "+ person.getAge());
-        System.out.println( "Id: " +  person.getId());
+
+        System.out.println("Antes de hacer la división");
+        //Excepciones
+        try {
+            division = numerador / denominador;
+        } catch (NullPointerException ex){
+            System.out.println("NullPointer");
+            System.out.println(ex.getMessage());
+        }catch ( Exception ex){
+            System.out.println("Exception");
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Después de la división");
+
+//        Ejercicio person= new Ejercicio("Santiago", 33, "102045455");
+////        person.setName("Santiago");
+////        person.setAge(33);
+////        person.setId("102045455");
+//        System.out.println( "Name: " + person.getName());
+//        System.out.println( "Age: "+ person.getAge());
+//        System.out.println( "Id: " +  person.getId());
 
 
 
